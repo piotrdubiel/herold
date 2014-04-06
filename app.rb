6a13cad5fa678ea641a93b1d6e75fcd28668b671
Piotrek Dubiel <piotr.dubiel@polidea.com>
2014-04-06 14:26:33 +0200

    init

---
+++ app.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+require "pubnub"
+
+publish_key   = ENV['PUBNUB_PUBLISH_KEY']
+subscribe_key = ENV['PUBNUB_SUBSCRIBE_KEY']
+
+pubnub = Pubnub.new(
+     :subscribe_key    => subscribe_key,
+     :publish_key      => publish_key,
+)
+
+
+get "/computer/on" do
+  pubnub.publish(channel: 'home', message: '{"object": "computer", "action": "on"') do |data|
+    puts data.response
+  end
+end
